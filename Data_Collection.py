43ce7cadad2a4a6934269202e259d4ad2fb6cbfa
VincentChen2 <chen.vincent.japan@gmail.com>
2024-03-13 18:39:21 -0400

    Updated Data_Collection.py

--- Data_Collection.py
+++ Data_Collection.py
@@ -1,10 +1,14 @@
 import cv2
+import os
 
 cam = cv2.VideoCapture(0).set(4, 100) #Sets up camera capture using front-facing camera
-number_of_images, current_images = input('Input Number of Images'), 0
+number_of_images, current_images = int(input('Input Number of Images: ')), 0
 folder_path = input('Input Folder Path: ')
 
-while current_images < number_of_images:
+if not os.path.exists(folder_path): #Creates directory if directory does not exist
+    os.makedirs(folder_path)
+
+while current_images < number_of_images: #Creates number of images, interrupts with 'q'
     _, frame = cam.read(0)
     cv2.imshow('Image', frame)
     cv2.write(f'{folder_path}/IMG_{current_images}')
@@ -12,3 +16,5 @@ while current_images < number_of_images:
     if cv2.waitKey(20) == ord('q'):
         break
     current_images += 1
+
+print(f'Created {current_images + 1} images at {folder_path}')
