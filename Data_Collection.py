fbcd5276c521450eb97f62ee5837cff73d2e888c
VincentChen2 <144965384+VincentChen2@users.noreply.github.com>
2024-03-27 18:50:55 -0400

    Fixed File Saving

--- Data_Collection.py
+++ Data_Collection.py
@@ -1,9 +1,15 @@
 import cv2
 import os
 
-cam = cv2.VideoCapture(1) #Sets up camera capture using front-facing camera
+cam = cv2.VideoCapture(1) #Sets up camera capture using front-facing camera !!!FOR MAC!!! (change if using other device)
 cam.set(3, 100) #Sets Width
 cam.set(4, 100) #Sets Height
+
+# Check if the camera opened successfully
+if not cam.isOpened():
+    print("Error: Camera could not be accessed.")
+    exit()
+
 number_of_images, current_images = int(input('Input Number of Images: ')), 0
 folder_path = input('Input Folder Path: ')
 
@@ -13,10 +19,13 @@ if not os.path.exists(folder_path): #Creates directory if directory does not exi
 while current_images < number_of_images: #Creates number of images, interrupts with 'q'
     _, frame = cam.read(0)
     cv2.imshow('Image', frame)
-    cv2.write(f'{folder_path}/IMG_{current_images}')
+    cv2.imwrite(f'{folder_path}/IMG_{current_images}.jpg', frame)
 
     if cv2.waitKey(20) == ord('q'):
         break
     current_images += 1
+
+cam.release()
+cv2.destroyAllWindows()
 
 print(f'Created {current_images + 1} images at {folder_path}')
